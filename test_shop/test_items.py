095c7a56fc9b72f38171c3177003fd6456795030
maglenka <42242054+maglenka@users.noreply.github.com>
2020-11-21 00:23:59 +0300

    add assert basket batton

--- test_shop/test_items.py
+++ test_shop/test_items.py
@@ -1,6 +1,17 @@
-link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
+import time
+import pytest
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 def test_guest_should_see_basket_button(browser):
     browser.get(link)
-    browser.find_element_by_css_selector("#add_to_basket_form")
+    time.sleep(5)
+    try:
+        browser.find_element_by_css_selector("#add_to_basket_form")
+        basket_btn = True
+        return basket_btn
+    except:
+        basket_btn = False
+
+    assert basket_btn, 'Button is missing on the page'
