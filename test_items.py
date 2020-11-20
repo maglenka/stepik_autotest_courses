935fba0439a58d9e6af6fc59ca6aedc388be67e6
maglenka <mag.fs4@gmail.com>
2020-11-20 21:55:39 +0300

    increased sleep time to 30 sec

--- test_items.py
+++ test_items.py
@@ -6,7 +6,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_guest_should_see_basket_button(browser):
     browser.get(link)
-    time.sleep(5)
+    time.sleep(30)
     try:
         browser.find_element_by_css_selector("#add_to_basket_form")
         basket_btn = True
