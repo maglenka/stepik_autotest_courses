1d7bd4c3c86806ba1d760186d4a33d888ee3d63d
maglenka <mag.fs4@gmail.com>
2020-11-30 11:43:17 +0300

    Added def assert for login and registration forms.

---
+++ pages/login_page.py
@@ -0,0 +1,28 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        cur_url = str(self.browser.current_url)
+        assert "login" in cur_url, "Substring 'login' not in current url"
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.BLOCK_HEADER), "Block header is not presented"
+        assert self.is_element_present(*LoginPageLocators.EMAIL_FORM), "Field 'Email address' is not presented"
+        assert self.is_element_present(*LoginPageLocators.PASSWORD_FORM), "Field 'Password' is not presented"
+        assert self.is_element_present(*LoginPageLocators.PASSWORD_RESET), "Link 'password reset' is not presented"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_BUTTON), "Log in button is not presented"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.BLOCK_HEADER_REG), "Block header is not presented"
+        assert self.is_element_present(*LoginPageLocators.EMAIL_FORM_REG), "Field 'Email address' is not presented"
+        assert self.is_element_present(*LoginPageLocators.PASSWORD_FORM_REG), "Field 'Password' is not presented"
+        assert self.is_element_present(*LoginPageLocators.PASSWORD_CONFIRM_FORM_REG), "Field 'Confirm password'" \
+                                                                                      " is not presented"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_BUTTON), "Log in button is not presented"
