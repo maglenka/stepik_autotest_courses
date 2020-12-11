0e0cf6498891e7b998c09d5efac73fc309b05d31
maglenka <mag.fs4@gmail.com>
2020-12-11 23:10:27 +0300

    added basket page

---
+++ pages/basket_page.py
@@ -0,0 +1,19 @@
+from .base_page import BasePage
+from .locators import BasketPageLocators
+
+
+class BasketPage(BasePage):
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*BasketPageLocators.LOGIN_LINK)
+        login_link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasketPageLocators.LOGIN_LINK), "Login link is not presented"
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasketPageLocators.LOGIN_LINK)
+        link.click()
+
+    def should_be_empty_basket(self):
+        assert self.is_not_element_present(*BasketPageLocators.ITEM_IN_CART), "There are items in the cart."
+        assert self.is_element_present(*BasketPageLocators.EMPTY_MESSAGE), "Text is not presented"
