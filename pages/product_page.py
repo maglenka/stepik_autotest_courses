9083a06a207236c67c52ce92bd52bf6998b0ae3e
maglenka <mag.fs4@gmail.com>
2020-12-11 21:56:19 +0300

    added a function to check the presence of elements

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,15 +1,12 @@
 from .base_page import BasePage
 from .locators import ProductPageLocators
-import time
 
 
 class ProductPage(BasePage):
     def add_product_to_basket(self):
         product_page_button = self.browser.find_element(*ProductPageLocators.ADD_BUTTON)
         product_page_button.click()
-        time.sleep(1)
         BasePage.solve_quiz_and_get_code(self)
-        time.sleep(3)
 
     def check_alert_content(self):
         name_of_book = self.browser.find_element(*ProductPageLocators.BOOK_NAME_IN_CATALOGUE).text
@@ -23,3 +20,11 @@ class ProductPage(BasePage):
         assert self.is_element_present(*ProductPageLocators.BOOK_NAME_IN_ALERT), "The message is not displayed."
         alert_price_of_book = self.browser.find_element(*ProductPageLocators.PRICE_IN_ALERT).text
         assert alert_price_of_book == price_of_book, "Cart value does not match the product price"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def should_dissapear_of_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
