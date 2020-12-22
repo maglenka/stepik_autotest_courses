2fa92a3ec894948a54d36a5ae37afa50ac680f4a
maglenka <mag.fs4@gmail.com>
2020-12-22 19:55:42 +0300

    Added tests for user. Marked some tests as "need_review"

--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,9 @@
 from .pages.product_page import ProductPage
 from .pages.basket_page import BasketPage
+from .pages.login_page import LoginPage
+from .pages.base_page import BasePage
 import pytest
+import time
 
 
 # @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
@@ -14,6 +17,7 @@ import pytest
 #                                                marks=pytest.mark.xfail),
 #                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
 #                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+@pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     page = ProductPage(browser, link)
@@ -54,6 +58,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page (browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)
@@ -61,8 +66,35 @@ def test_guest_can_go_to_login_page_from_product_page (browser):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)
     page.open()
     BasketPage.basket_should_be_empty
+
+
+class TestUserAddToBasketFromProductPage(object):
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+        self.page = LoginPage(browser, link)
+        self.page.open()
+        time.sleep(2)
+        LoginPage.register_new_user(self.page, "test" + str(time.time()) + "@test.ru", "1qaz@WSX3edc")
+        BasePage.should_be_authorized_user
+
+    @pytest.mark.need_review
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_product_to_basket()
+        page.check_alert_content()
+
+    @pytest.mark.need_review
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
