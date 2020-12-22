8b5e8ec3ff51345c8645f6224de601cbdf8d4f74
maglenka <mag.fs4@gmail.com>
2020-12-22 18:57:50 +0300

    Update BasePageLocators - added user icon selector

--- pages/locators.py
+++ pages/locators.py
@@ -35,6 +35,7 @@ class BasePageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
     BASKET_BUTTON = (By.CSS_SELECTOR, "div.basket-mini a.btn.btn-default")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 
 class BasketPageLocators:
